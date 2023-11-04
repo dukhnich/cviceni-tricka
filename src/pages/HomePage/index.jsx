dec09b26225cf9b9836c714a72175b6addccc6b5
Maryna Dukhnich <maryna.dukhnich@gmail.com>
2023-11-04 17:05:07 +0100

    add changing functionality

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,17 +1,23 @@
+import { useState, useEffect } from 'react';
 import { TShirt } from '../components/TShirt';
 import { TShirtSetup } from '../components/TShirtSetup';
 import './style.css';
 
 export const HomePage = () => {
+  const [settings, setSettings] = useState({
+    type: 'normalShortSleeve',
+    color: 'yellow',
+    print: 'Ahoj světe'
+  })
   return (
     <div className="container">
       <div className="tshirt-panel">
         <div className="tshirt-panel__tshirt">
-          <TShirt type="normalShortSleeve" color="yellow" print="Ahoj světe" />
+          <TShirt {...settings} />
         </div>
         <div className="tshirt-panel__setup">
           <h2>Možnosti:</h2>
-          <TShirtSetup />
+          <TShirtSetup settings={settings} onChange={setSettings} />
         </div>
       </div>
     </div>
--- src/pages/components/TShirtSetup/index.jsx
+++ src/pages/components/TShirtSetup/index.jsx
@@ -1,10 +1,11 @@
 import './style.css';
 
-export const TShirtSetup = () => {
+export const TShirtSetup = ({settings, onChange}) => {
+  const changeField = (part) => onChange({...settings, ...part})
   return (
     <form className="tshirt-setup">
       <label className="tshirt-setup__label">Typ:</label>
-      <select className="tshirt-setup__input">
+      <select className="tshirt-setup__input" value={settings.type} onChange={e => changeField({type: e.target.value})} >
         <option value="normalShortSleeve">Krátký rukáv</option>
         <option value="tallShortSleeve">Krátký rukáv (prodloužené)</option>
         <option value="normalLongSleeve">Dlouhý rukáv</option>
@@ -14,7 +15,7 @@ export const TShirtSetup = () => {
       </select>
 
       <label className="tshirt-setup__label">Barva:</label>
-      <select className="tshirt-setup__input">
+      <select className="tshirt-setup__input" value={settings.color} onChange={e => changeField({color: e.target.value})}>
         <option value="yellow">Žlutá</option>
         <option value="red">Červená</option>
         <option value="blue">Modrá</option>
@@ -22,7 +23,7 @@ export const TShirtSetup = () => {
       </select>
     
       <label className="tshirt-setup__label">Potisk:</label>
-      <input className="tshirt-setup__input" type="text" defaultValue="Ahoj světe" />
+      <input className="tshirt-setup__input" type="text" value={settings.print} onChange={e => changeField({print: e.target.value})}/>
     </form>
   );
 };
